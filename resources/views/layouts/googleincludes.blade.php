8473c981ac05215fd83e3289d3038b8e2f34690d
Dries Goossens <driesgoossens93@gmail.com>
2018-10-19 11:45:13 +0200

    adding google adsense

--- resources/views/layouts/googleincludes.blade.php
+++ resources/views/layouts/googleincludes.blade.php
@@ -7,3 +7,10 @@
 
   gtag('config', 'UA-127790595-1');
 </script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-5322935690205066",
+    enable_page_level_ads: true
+  });
+</script>
